f659bf27aacd95c78b3293934cf487f4856d1fb9
Sebastian <sebastian.debesa@hotmail.com>
2024-04-23 14:24:11 -0300

    update Tablero now is functional

--- src/Tablero.java
+++ src/Tablero.java
@@ -1,3 +1,22 @@
+import java.util.Arrays;
+
 public class Tablero {
-    String [][] tablero = new String[3][3];
+    static char [][] tablero = new char[3][3];
+    public static void mostrarTablero(){
+        for (int i = 0; i < 3 ; i++){
+            System.out.println(tablero[i][0] + " " + tablero[i][1]+ " " + tablero[i][2]);
+        }
+
+    }
+
+    public static void limpiarTablero(){
+        for (char[] fila : tablero){
+            Arrays.fill(fila,'-');
+        }
+    }
+
+    public static void main(String[] args) {
+        limpiarTablero();
+        mostrarTablero();
+    }
 }
