29d092b797d3500f67316d972b65b1612d093dde
Sebastian <sebastian.debesa@hotmail.com>
2024-04-24 14:15:33 -0300

    update TaTeTi and Main, the game is playable

--- src/TaTeTi.java
+++ src/TaTeTi.java
@@ -1,15 +1,60 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class TaTeTi {
     boolean hayGanador = false;
     Scanner scan = new Scanner(System.in);
 
-    public void hayGanador() {
-
-
+    public void hayGanador(){
         hayGanador = true;
         scan.close();
+        System.out.println("¡El ganador es: " + Jugador.turno + "!");
+    }
+
+    public void algunGanador() {
+        //condiciones de victoria
+
+        //ganador en las fila
+        for(int i = 0; i <3; i++){
+            if (Tablero.posiciones[i][0] == Tablero.posiciones[i][1] &&
+                    Tablero.posiciones[i][1] == Tablero.posiciones[i][2] &&
+                    Tablero.posiciones[i][0] != '-' ){
+                System.out.println("gano porfilas");
+                hayGanador();
+            }
+        }
+        //ganador en las columnas
+        for (int i = 0; i <3 ; i++){
+            if (Tablero.posiciones[0][i] == Tablero.posiciones[1][i] &&
+                 Tablero.posiciones[0][i] == Tablero.posiciones[2][i] &&
+                    Tablero.posiciones[0][i] != '-'){
+                hayGanador();
+            }
+        }
+        //ganador en diagonal
+        if ( Tablero.posiciones[0][0] == Tablero.posiciones[1][1] &&
+                Tablero.posiciones[0][0] == Tablero.posiciones[2][2] &&
+                Tablero.posiciones[0][0] != '-'){
+            hayGanador();
+        }
+        if (Tablero.posiciones[0][2] == Tablero.posiciones[1][1] &&
+                Tablero.posiciones[0][2] == Tablero.posiciones[2][0] &&
+                Tablero.posiciones[0][2] != '-'){
+            hayGanador();
+        }
+    }
+
+    public void hayEmpate(){
+        int contadorDeTurnos = 0;
+        for (int i = 0; i <3 ;i++){
+            for (int j =0; j<3; j++){
+                if (Tablero.posiciones[i][j] != '-' ){
+                    contadorDeTurnos++;
+                }
+            }
+        }
+        if (contadorDeTurnos ==9){
+            System.out.println("Es un empate.");
+        }
     }
 
     public void jugar(){
@@ -27,15 +72,15 @@ public class TaTeTi {
                 System.out.println("Turno del jugador " + (Jugador.turno));
 
                 //captura de posicion por el jugador
-                System.out.println("Ingresa tu posicion en el eje horizontal:");
-                int movimientoHorizontal = Integer.parseInt(scan.nextLine());
-
                 System.out.println("Ingresa tu posicion en el eje vertical:");
                 int movimientoVertical = Integer.parseInt(scan.nextLine());
 
+                System.out.println("Ingresa tu posicion en el eje horizontal:");
+                int movimientoHorizontal = Integer.parseInt(scan.nextLine());
+
                 //comprueba si la posicion está vacia
-                if (Tablero.posiciones[movimientoHorizontal][movimientoVertical] == '-') {
-                    Tablero.posiciones[movimientoHorizontal][movimientoVertical] = Jugador.turno;
+                if (Tablero.posiciones[movimientoVertical][movimientoHorizontal] == '-') {
+                    Tablero.posiciones[movimientoVertical][movimientoHorizontal] = Jugador.turno;
                 } else {
                     System.out.println("Este valor ya está en uso. Vuelve a intentarlo");
                     continue;
@@ -43,10 +88,14 @@ public class TaTeTi {
 
                 //muestra el tablero
                 Tablero.mostrarTablero();
+                algunGanador();
+                hayEmpate();
                 Jugador.cambiarTurno();
 
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Parece que usaste un indice de movimiento mayor a 2. Vuelve a intentarlo.");
+            } catch (NumberFormatException e){
+                System.out.println("Debes ingresar un numero valido entre 0 y 2 . Vuelve a intentarlo.");
             }
         }
 
